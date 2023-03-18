1157691ee7d1fa5b87e4690562e431b5da5af353
jeremyd <4072+jeremyd@users.noreply.github.com>
2023-03-18 06:30:13 -0700

    support alby webln ext

--- app/invoices/invoice.tsx
+++ app/invoices/invoice.tsx
@@ -1,33 +1,50 @@
-"use client"
 import { useState, useEffect, useRef } from 'react';
 import QRCode from 'qrcode';
 import React from 'react';
 
+export async function alby(lnurl) {
+    // const lnurl = (provided by your application backend)
+    try {
+        await webln.enable();
+        const result = await webln.sendPayment(lnurl); // promise resolves once the LNURL process is finished 
+    } catch (error) {
+        console.log("something went wrong with webln: " + error)
+    }
+}
+
 export default function Bolt11Invoice(
     props: React.PropsWithChildren<{
         payment_request: string;
-    }>) {
-    console.log(props.payment_request)
+    }>
+) {
+    const [bolt, setBolt] = useState<string>(props.payment_request);
+    const qrCodeRef = useRef<HTMLCanvasElement>(null);
 
-    const qrCodeRef = useRef(null);
-
-    const bolt = props.payment_request
+    alby(bolt);
 
     useEffect(() => {
         if (bolt && qrCodeRef.current) {
-            QRCode.toCanvas(qrCodeRef.current, bolt, { width: 256 }, function (error) {
-                if (error) console.error(error);
-            });
+            QRCode.toCanvas(
+                qrCodeRef.current,
+                bolt,
+                { width: 256 },
+                function (error) {
+                    if (error) console.error(error);
+                }
+            );
         }
     }, [bolt]);
 
+    function copyToClipboard() {
+        navigator.clipboard.writeText(bolt).then(() => {
+            console.log('Copied to clipboard!');
+        });
+    }
+
     return (
         <div>
-            {bolt ? (
-                <canvas ref={qrCodeRef} />
-            ) : (
-                <p>Loading invoice...</p>
-            )}
+            <canvas ref={qrCodeRef} />
+            <button onClick={copyToClipboard}>Copy to clipboard</button>
         </div>
     );
 }
