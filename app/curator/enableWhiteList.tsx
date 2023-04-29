5be9a458d78b2e09709708b72c4034c7472f03d2
jeremyd <4072+jeremyd@users.noreply.github.com>
2023-04-28 17:17:03 -0700

    adding more buttons

---
+++ app/curator/enableWhiteList.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { useRouter } from 'next/navigation'
+
+type Relay = {
+    id: string;
+}
+
+export default function EnableWhiteList(props: React.PropsWithChildren<{
+    relay: Relay;
+}>) {
+    const router = useRouter();
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        const id = event.currentTarget.id
+        console.log(event.currentTarget.id)
+        // call to API to create a new blank whitelist and re-render page
+        const response = await fetch(`/api/relay/${id}/whitelist`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+        });
+        router.push(`/curator?relay_id=${id}`)
+    }
+
+    return (
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <button
+                onClick={(e) => handleSubmit(e)}
+                id={props.relay.id}
+                type="button"
+                className="block rounded-md bg-purple-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+            >
+                Enable Whitelist
+            </button>
+        </div>
+    )
+}
--- app/curator/page.tsx
+++ app/curator/page.tsx
@@ -3,8 +3,10 @@ import authOptions from "../../pages/api/auth/[...nextauth]"
 import prisma from '../../lib/prisma'
 import ListEntryKeywords from "./listEntryKeywords"
 import ListEntryPubkeys from "./listEntryPubkeys"
+import EnableWhiteList from "./enableWhiteList"
+import EnableBlackList from "./enableBlackList"
 
-export default async function Curator(searchParams: Record<string, string>) {
+export default async function Curator(searchParams: Record<string, Record<string, string>>) {
     const session = await getServerSession(authOptions)
 
     if (!session || !(session as any).user.name) {
@@ -48,11 +50,30 @@ export default async function Curator(searchParams: Record<string, string>) {
     return (
         <div>
             <h1>{relay?.name}</h1>
-            <ListEntryKeywords keywords={relay?.white_list.list_keywords} kind="Whitelisted keywords"></ListEntryKeywords>
-            <ListEntryKeywords keywords={relay?.black_list.list_keywords} kind="Blacklisted keywords"></ListEntryKeywords>
-            <ListEntryPubkeys pubkeys={relay?.white_list.list_pubkeys} kind="Whitelisted pubkeys"></ListEntryPubkeys>
-            <ListEntryPubkeys pubkeys={relay?.black_list.list_pubkeys} kind="Blacklisted pubkeys"></ListEntryPubkeys>
 
+            {relay != null && relay.white_list == null &&
+                <EnableWhiteList relay={relay}></EnableWhiteList>
+            }
+
+            {relay != null && relay.black_list == null &&
+                <EnableBlackList relay={relay}></EnableBlackList>
+            }
+
+            {relay != null && relay.white_list != null &&
+                <ListEntryKeywords keywords={relay.white_list.list_keywords} kind="Whitelisted keywords"></ListEntryKeywords>
+            }
+
+            {relay != null && relay.black_list != null &&
+                <ListEntryKeywords keywords={relay.black_list.list_keywords} kind="Blacklisted keywords"></ListEntryKeywords>
+            }
+
+            {relay != null && relay.white_list != null &&
+                <ListEntryPubkeys pubkeys={relay.white_list.list_pubkeys} kind="Whitelisted pubkeys"></ListEntryPubkeys>
+            }
+
+            {relay != null && relay.black_list != null &&
+                <ListEntryPubkeys pubkeys={relay.black_list.list_pubkeys} kind="Blacklisted pubkeys"></ListEntryPubkeys>
+            }
         </div>
     )
 }
