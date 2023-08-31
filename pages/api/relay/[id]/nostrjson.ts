5cdafd9d077826b77c97d6bcf15d7793b1f7bc10
jeremyd <4072+jeremyd@users.noreply.github.com>
2023-08-30 23:22:17 -0700

    adding additional nip11 fields for payment_required and limitations

--- pages/api/relay/[id]/nostrjson.ts
+++ pages/api/relay/[id]/nostrjson.ts
@@ -15,7 +15,7 @@ export default async function handle(req: any, res: any) {
         return
     }
 
-    const nostrjson = {
+    let nostrjson: any = {
         "description": relay.details,
         "name": relay.name + "." + relay.domain,
         "pubkey": relay.owner.pubkey,
@@ -24,6 +24,29 @@ export default async function handle(req: any, res: any) {
         "version": "0.9.3",
         "posting_policy": "https://" + relay.name + "." + relay.domain + "#policy",
     }
+
+    if (relay.payment_required == true) {
+        nostrjson["limitation"] = { "payment_required": true }
+        nostrjson["payments_url"] = "https://" + relay.name + "." + relay.domain
+        nostrjson["fees"] = {
+            "admission": [{ "amount": relay.payment_amount, "unit": "sats" }],
+        }
+    }
+
+    nostrjson["limitation"]["max_message_length"] = 262200
+    nostrjson["limitation"]["max_subscriptions"] = 80
+    nostrjson["limitation"]["max_filters"] = 256 // unsure
+    nostrjson["limitation"]["max_limit"] = 10000
+    //nostrjson["limitation"]["max_subid_length"] = unknown
+    nostrjson["limitation"]["max_event_tags"] = 2000
+    nostrjson["limitation"]["max_content_length"] = 262140
+    nostrjson["limitation"]["min_pow_difficulty"] = 0
+    nostrjson["limitation"]["auth_required"] = false
+
+    // nip11 pull-req pending for created_at limits
+    // https://github.com/nostr-protocol/nips/pull/756
+    nostrjson["limitation"]["created_at_lower_limit"] = 94608000
+    nostrjson["limitation"]["created_at_upper_limit"] = 900
 
     res.status(200).json(nostrjson)
 }
