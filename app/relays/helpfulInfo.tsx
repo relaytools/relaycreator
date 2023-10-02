d5c142480da3c2fd7c544b439dbbbb4e9f544f90
jeremyd <jeremyd>
2023-10-02 14:59:17 -0700

    more messing around with wording and sections

--- app/relays/helpfulInfo.tsx
+++ app/relays/helpfulInfo.tsx
@@ -2,6 +2,7 @@
 import { useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { IoArrowForwardOutline } from 'react-icons/io5';
+import { IoLogoGithub } from 'react-icons/io5';
 
 export default function HelpfulInfo(props: React.PropsWithChildren<{}>) {
     const { data: session, status } = useSession();
@@ -29,7 +30,7 @@ export default function HelpfulInfo(props: React.PropsWithChildren<{}>) {
 
     return (
         <div className="font-jetbrains flex flex-col justify-center items-center">
-            <h1 className="justify-center text-3xl text-primary">The fastest and easiest way to create nostr relays</h1>
+            <h1 className="justify-center text-3xl text-primary">"relays are best served hot"</h1>
             <div className="mt-2 lg:grid lg:grid-cols-3 lg:gap-2 sm:flex sm:flex-col sm:gap-4">
                 <div className="card w-96 bg-base-100 hidden lg:block">
                     <div className="card-body">
@@ -48,13 +49,12 @@ export default function HelpfulInfo(props: React.PropsWithChildren<{}>) {
                 </div>
                 <div className="card w-96 bg-base-100">
                     <div className="card-body">
-                        <h2 className="card-title">relay creator</h2>
-
-                        <p>ready right now?</p>
+                        <h2 className="card-title">products: relay creator</h2>
                         <p>pick a name, create the relay, it's simple and quick</p>
                         <p>pricing: 21,000 sats/month</p>
                         <p></p>
                         <p><a className="link" href="/faq">learn more</a></p>
+                        <a href="https://github.com/relaytools"><IoLogoGithub /></a>
                     </div>
                 </div>
             </div>
@@ -66,6 +66,6 @@ export default function HelpfulInfo(props: React.PropsWithChildren<{}>) {
                     Create a relay<span className="fl pl-2"><IoArrowForwardOutline /></span>
                 </a>
             </div>
-        </div>
+        </div >
     )
 }
