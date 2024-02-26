07e36506f3d914bc24b499a1ee78de94d645313c
jeremyd <4072+jeremyd@users.noreply.github.com>
2024-02-26 13:56:34 -0800

    listEntryPubkeys adding: delete-all, list synchronization.  invoices page: re-design

--- app/invoices/balances.tsx
+++ app/invoices/balances.tsx
@@ -1,14 +1,14 @@
-"use client"
-import { useSession } from 'next-auth/react';
-import { useSearchParams } from 'next/navigation';
-import Bolt11Invoice from '../components/invoice'
-import {useState} from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import Bolt11Invoice from "../components/invoice";
+import { use, useState } from "react";
+import { useRouter } from "next/navigation";
 
 function copyToClipboard(e: any, bolt: string) {
-    e.preventDefault()
+    e.preventDefault();
     navigator.clipboard.writeText(bolt).then(() => {
-        console.log('Copied to clipboard!');
+        console.log("Copied to clipboard!");
     });
 }
 
@@ -16,74 +16,171 @@ export async function alby(lnurl: string) {
     // const lnurl = (provided by your application backend)
     try {
         await (window as any).webln.enable();
-        const result = await (window as any).webln.sendPayment(lnurl); // promise resolves once the LNURL process is finished 
+        const result = await (window as any).webln.sendPayment(lnurl); // promise resolves once the LNURL process is finished
     } catch (error) {
-        console.log("something went wrong with webln: " + error)
+        console.log("something went wrong with webln: " + error);
     }
 }
-
-
 
 export default function Balances(
     props: React.PropsWithChildren<{
         RelayBalances: any;
         IsAdmin: boolean;
-    }>) {
+    }>
+) {
+    const router = useRouter();
+    const [showOrders, setShowOrders] = useState("");
 
-        const router = useRouter()
+    async function getTopUpInvoice(b: any) {
+        const response = await fetch(
+            `/api/invoices?relayname=${b.relayName}&topup=true`
+        );
+        const responseJson = await response.json();
+        console.log(responseJson);
 
-        async function getTopUpInvoice(b: any) {
-
-            const response = await fetch(`/api/invoices?relayname=${b.relayName}&topup=true`);
-            const responseJson = await response.json();
-            console.log(responseJson);
-
-            if(response.ok) {
-                router.push(`/invoices?relayname=${b.relayName}&order_id=${responseJson.order_id}&pubkey=unknown`);
-            }
+        if (response.ok) {
+            router.push(
+                `/invoices?relayname=${b.relayName}&order_id=${responseJson.order_id}&pubkey=unknown`
+            );
         }
+    }
 
-        //const sortedRelays = props.RelayBalances.sort((a: any, b: any) => a.owner.localeCompare(b.owner));
-        const sortedRelays = props.RelayBalances.sort((a: any, b: any) => {
+    function showOrdersFor(b: any) {
+        if (showOrders === b) {
+            return true;
+        }
+        return false;
+    }
+
+    //const sortedRelays = props.RelayBalances.sort((a: any, b: any) => a.owner.localeCompare(b.owner));
+    const sortedRelays = props.RelayBalances.sort((a: any, b: any) => {
         const ownerComparison = a.owner.localeCompare(b.owner);
         return ownerComparison !== 0 ? ownerComparison : a.balance - b.balance;
-        });
+    });
 
-        return (
-            <div>
-            <h1>Balances</h1>
-                    <div className="mt-8 flow-root">
-                        <div className="overflow-x-auto">
-                            <table className="table table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>Relay Name</th>
-                                        <th>Paid Relay bonus (sats)</th>
-                                        <th>Remaining Balance (sats)</th>
-                                        {props.IsAdmin && <th>Owner (pubkey)</th>}
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    {sortedRelays.map((b: any) => (
-                                        <tr key={b.relayId + "rowkey"}>
-                                            <td>{b.relayName}</td>
-                                            <td>{b.clientPayments}</td>
-                                            <td>{b.balance}</td>
-                                            {props.IsAdmin && <td>{b.owner}</td>}
-                                            <td>
-                                                <button className="btn btn-secondary"  
-                                                onClick={() => getTopUpInvoice(b)}>
-                                                    top up balance
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                  </div>  
-                  </div>
-        )
+    function amountPrecision(amount: number) {
+        return Math.round(amount);
     }
+
+    return (
+        <div>
+            <h1>Balances</h1>
+            <div className="mt-4 font-jetbrains">
+                {sortedRelays.map((b: any) => (
+                    <div
+                        key={b.relayId + "rowkey"}
+                        className="flex flex-col border mb-4 bg-gradient-to-r from-accent to-base-100 p-4 "
+                    >
+                        <div className="flex">
+                            <div className="w-1/2 border-b">Relay Name</div>
+                            <div className="w-1/2 border-b text-lg">{b.relayName}</div>
+                        </div>
+                        <div className="flex">
+                            <div className="w-1/2">Relay Status</div>
+                            <div className="w-1/2">{b.relayStatus}</div>
+                        </div>
+                        <div className="flex">
+                            <div className="w-1/2">Paid Relay Bonus</div>
+                            <div className="w-1/2">{b.clientPayments} sats</div>
+                        </div>
+                        <div className="flex">
+                            <div className="w-1/2">Remaining Balance</div>
+                            <div className="w-1/2">
+                                {amountPrecision(b.balance)} sats
+                            </div>
+                        </div>
+                        <div className="flex">
+                            {props.IsAdmin && (
+                                <div className="w-1/2">Owner (pubkey)</div>
+                            )}
+                            {props.IsAdmin && (
+                                <div className="w-1/2">{b.owner}</div>
+                            )}
+                        </div>
+                        <div className="flex mt-4">
+                            <button
+                                className="mr-2 btn btn-secondary"
+                                onClick={() => setShowOrders(b.relayId)}
+                            >
+                                show orders
+                            </button>
+                            <button
+                                className="btn btn-secondary"
+                                onClick={() => getTopUpInvoice(b)}
+                            >
+                                top up
+                            </button>
+                        </div>
+
+                        {showOrdersFor(b.relayId) &&
+                            b.unpaidOrders.map((order: any) => (
+                                <div key={order.id + "colkey"} className="flex-col border">
+                                    <div className="flex">
+                                    <div className="w-1/3 mr-2">
+                                        Pending Order
+                                    </div>
+                                    <div className="w-1/3 mr-2">
+                                        {order.amount} sats
+                                    </div>
+                                    <a
+                                        className="btn btn-secondary"
+                                        href={`/invoices?relayname=${b.relayName}&pubkey=${b.pubkey}&order_id=${order.id}`}
+                                    >
+                                        show
+                                    </a>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2 mr-2 text-lg">
+                                            {b.relayName}
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2">Expires At</div>
+                                        <div className="w-1/2">
+                                            {order.expires_at
+                                                ? new Date(
+                                                      order.expires_at
+                                                  ).toLocaleString()
+                                                : ""}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+
+                        {showOrdersFor(b.relayId) &&
+                            b.orders.map((order: any) => (
+                                <div
+                                    key={order.id + "colkey"}
+                                    className="flex flex-col border"
+                                >
+                                    <div className="flex">
+                                        <div className="w-1/2 mr-2">
+                                            Paid Order
+                                        </div>
+                                        <div className="w-1/2 mr-2">
+                                            {amountPrecision(order.amount)} sats
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2 mr-2 text-lg">
+                                            {b.relayName}
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2">Paid At</div>
+                                        <div className="w-1/2">
+                                            {order.paid_at
+                                                ? new Date(
+                                                      order.paid_at
+                                                  ).toLocaleString()
+                                                : ""}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
--- pages/api/relay/[id]/allowlistpubkeys.ts
+++ pages/api/relay/[id]/allowlistpubkeys.ts
@@ -30,6 +30,26 @@ export default async function handle(req: any, res: any) {
             return
         }
 
+        const curPubkeys = await prisma.listEntryPubkey.findMany({
+            where: {
+                AllowListId: allow_list.id
+            }
+        })
+
+        // if list of pubkeys has reason = "list:<something>" then sync the list.
+        // by deleting the pubkeys that are not in the list anymore
+        if(reason.startsWith("list:")) {
+            for(const pk of curPubkeys) {
+                if(pk.reason == reason) {
+                    await prisma.listEntryPubkey.delete({
+                        where: {
+                            id: pk.id
+                        }
+                    })
+                }
+            }
+        }
+
         let newPubkeys = []
         for (const pk of pubkeys) {
             const newp = await prisma.listEntryPubkey.create({
@@ -42,6 +62,26 @@ export default async function handle(req: any, res: any) {
             newPubkeys.push(newp)
         }
         res.status(200).json({ "pubkeys": newPubkeys });
+    // DELETE ALL
+    } else if (req.method == "DELETE") {
+        if (allow_list == null || allow_list.id == null) {
+            res.status(500).json({ "error": "allow_list is null" })
+            return
+        }
+
+        const listId = req.query.list_id;
+        if (listId == null || listId != "all") {
+            res.status(500).json({ "error": "no list_id == all" })
+            return
+        }
+
+        await prisma.listEntryPubkey.deleteMany({
+            where: {
+                AllowListId: allow_list.id
+            }
+        })
+
+        res.status(200).json({});
     } else {
         res.status(500).json({ "error": "method not allowed" })
     }
