57f21f97ca0a4120e100f82a38d7c2183e6d5ac7
jeremyd <jeremyd>
2024-02-26 13:56:34 -0800

    listEntryPubkeys adding: delete-all, list synchronization.  invoices page: re-design

--- pages/api/relay/[id]/blocklistpubkeys.ts
+++ pages/api/relay/[id]/blocklistpubkeys.ts
@@ -30,6 +30,26 @@ export default async function handle(req: any, res: any) {
             return
         }
 
+        const curPubkeys = await prisma.listEntryPubkey.findMany({
+            where: {
+                BlockListId: block_list.id
+            }
+        })
+
+        // if list of pubkeys has reason = "list:<something>" then sync the list.
+        // by deleting the pubkeys that are not in the list anymore
+        if(reason.startsWith("list:")) {
+            for(const pk of curPubkeys) {
+                if(pk.reason == reason) {
+                    await prisma.listEntryPubkey.delete({
+                        where: {
+                            id: pk.id
+                        }
+                    })
+                }
+            }
+        }
+
         let newPubkeys = []
         for (const pk of pubkeys) {
             const newp = await prisma.listEntryPubkey.create({
@@ -42,6 +62,27 @@ export default async function handle(req: any, res: any) {
             newPubkeys.push(newp)
         }
         res.status(200).json({ "pubkeys": newPubkeys });
+
+    // DELETE ALL
+    } else if (req.method == "DELETE") {
+        if (block_list == null || block_list.id == null) {
+            res.status(500).json({ "error": "block_list is null" })
+            return
+        }
+
+        const listId = req.query.list_id;
+        if (listId == null) {
+            res.status(500).json({ "error": "no list_id" })
+            return
+        }
+
+        await prisma.listEntryPubkey.deleteMany({
+            where: {
+                BlockListId: block_list.id,
+            }
+        })
+
+        res.status(200).json({});
     } else {
         res.status(500).json({ "error": "method not allowed" })
     }
