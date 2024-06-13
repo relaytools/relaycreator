52e51dec2dab41433b3da055035530f75ab8b134
jeremyd <jeremyd>
2024-06-13 00:56:45 -0700

    somehow we made the cards working better, save it!

--- app/components/relay.tsx
+++ app/components/relay.tsx
@@ -51,25 +51,26 @@ export default function Relay(
         useRelayHttps = "https://" + props.relay.domain
     }
 
+    let useDetails = ""
+    if(props.relay.details) {
+        useDetails = props.relay.details.split('\n').slice(0, 2).join('\n');
+    }
+
     const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN || "http://localhost:3000"
     return (
         <div id={props.relay.id + "rootview"} className="">
             {props.showDetail &&
                 <a href={useRelayHttps} className="">
-                    <div className="card w-96 shadow-xl text-white selectable mb-4 hover:bg-gray-800 hover:text-white hover:bg-opacity-80" style={{
+                    <div className="card rounded-none flex flex-col w-68 h-72 mr-12 mb-12 text-white selectable hover:bg-gray-800 hover:text-white hover:bg-opacity-80" style={{
                         backgroundImage: `url(${edited ? (profileBanner || "/green-check.png") : (props.relay.banner_image || "/green-check.png")})`,
                         backgroundSize: "cover",
                         textShadow: "0px 0px 5px rgba(0, 0, 0, 0.5)"
                     }}>
-                        <div className="card-body items-center justify-end">
-                            <div className="card h-48 w-96"></div>
-                            <div className="card h-48 w-96">
-                                <div className="card-body bg-black bg-opacity-80 hover:bg-gray-800 hover:text-white hover:bg-opacity-80">
-                                    <h2 className="card-title mr-4 ml-2 mt-2" style={{ whiteSpace: "pre-wrap", overflow: "auto" }}>{props.relay.name}</h2>
-                                    <p className="mb-2 mr-4 ml-2 mt-2" style={{ whiteSpace: "pre-wrap", overflow: "auto" }}>{useRelayWSS}</p>
-                                    <p className="mb-2 mr-4 ml-2 mt-2" style={{ whiteSpace: "pre-wrap", minHeight: "52px", maxHeight: "52px", overflow: "auto" }}>{edited ? (profileDetail || "") : (props.relay.details || "")}</p>
-                                </div>
-                            </div>
+                        <div className="flex-grow"/>
+                        <div className="card-body bg-black bg-opacity-80 hover:bg-gray-800 hover:text-white hover:bg-opacity-80 max-h-40">
+                            <h2 className="card-title" style={{ whiteSpace: "pre-line", overflow: "hidden" }}>{props.relay.name}</h2>
+                            <p className="text-sm" style={{ whiteSpace: "pre-line", overflow: "hidden" }}>{useRelayWSS}</p>
+                            <p className="text-sm" style={{ whiteSpace: "pre-line", overflow: "hidden" }}>{edited ? (profileDetail || "") : (useDetails)}</p>
                         </div>
                     </div>
 
