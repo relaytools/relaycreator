e3428d228bc3572538af67bedaa1d36d2bff98d8
jeremyd <4072+jeremyd@users.noreply.github.com>
2024-06-13 17:44:11 -0700

    closest weve gotten so far

--- app/components/relay.tsx
+++ app/components/relay.tsx
@@ -58,10 +58,10 @@ export default function Relay(
 
     const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN || "http://localhost:3000"
     return (
-        <div id={props.relay.id + "rootview"} className="">
+        <div id={props.relay.id + "rootview"} className="lg:w-1/2 sm:w-full">
             {props.showDetail &&
                 <a href={useRelayHttps} className="">
-                    <div className="card rounded-none flex flex-col w-68 h-72 mr-12 mb-12 text-white selectable hover:bg-gray-800 hover:text-white hover:bg-opacity-80" style={{
+                    <div className="card rounded-none h-72 mr-12 mb-12 text-white selectable hover:bg-gray-800 hover:text-white hover:bg-opacity-80" style={{
                         backgroundImage: `url(${edited ? (profileBanner || "/green-check.png") : (props.relay.banner_image || "/green-check.png")})`,
                         backgroundSize: "cover",
                         textShadow: "0px 0px 5px rgba(0, 0, 0, 0.5)"
