5fad2de26aec473c25a0c01ee3b2b84cb5e4cf79
jeremyd <jeremyd>
2024-06-25 11:20:21 -0700

    font tweaks to match penpot

--- app/relays/helpfulInfo.tsx
+++ app/relays/helpfulInfo.tsx
@@ -29,18 +29,18 @@ export default function HelpfulInfo(props: React.PropsWithChildren<{}>) {
 
     return (
         <div className="">
-            <h1 className="mt-8 text-5xl text-primary text-center">The fastest and easiest way to launch new relays for nostr.</h1>
+            <h1 className="mt-8 text-5xl text-primary text-center font-roboto">The fastest and easiest way to launch new relays for nostr.</h1>
             <div className="mt-8 flex flex-wrap gap-12">
                 <div className="bg-base-100 hidden flex-1 lg:flex-auto lg:block lg:w-1/4">
                     <div className="">
-                        <h2 className="font-bold text-xl mb-2">What is nostr?</h2>
-                        <p>Nostr is a magical journey where we discover how to take control over our social media experience. Relays are an important part of the nostr ecosystem.</p>
+                        <h2 className="text-xl mb-2">What is Nostr?</h2>
+                        <p className="font-roboto">Nostr is a distributed social media protocol for the internet aimed at taking back control over our social media experience. Relays are an important part of the nostr ecosystem.</p>
                     </div>
                 </div>
                 <div className="bg-base-100 flex-1 lg:flex-auto hidden lg:block lg:w-1/4">
                     <div className="">
-                        <h2 className="font-bold text-xl mb-2">Discover, Create, Join</h2>
-                        <p>
+                        <h2 className="font text-xl mb-2">Discover, Create, Join</h2>
+                        <p className="font-roboto">
                             You can discover new relays to connect to.
                             You can create new relays. You can join a relay as a paid member or a moderator.
                         </p>
@@ -48,11 +48,13 @@ export default function HelpfulInfo(props: React.PropsWithChildren<{}>) {
                 </div>
                 <div className="bg-base-100 flex-1 lg:flex-auto lg:w-1/4">
                     <div className="">
-                        <h2 className="font-bold text-xl mb-2">Relay Creator</h2>
-                        <p>Create relays of any type quickly and easily</p>
-                        <p>Pricing: 12,000 sats/month</p>
-                        <p className="">open source</p>
-                        <a className="link" href="https://github.com/relaytools">github</a>
+                        <h2 className="text-xl mb-2">Relay Creator</h2>
+                        <div className="font-roboto">
+                            <p>Create relays of any type quickly and easily</p>
+                            <p>Pricing: 12,000 sats/month</p>
+                            <p className="">open source</p>
+                            <a className="link" href="https://github.com/relaytools">github</a>
+                        </div>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,18 @@ module.exports = {
       },
     extend: {
       fontFamily: {
-        jetbrains: 'JetBrains-Mono-Thin'
+        jetbrains: ["var(--font-jetbrains)"],
+        sans: ["var(--font-open-sans)"],
+        mono: ["var(--font-roboto-mono)"],
+        condensed: ["var(--font-roboto-condensed)"],
       }
+    },
+    fontSize: {
+        base: '1.25rem', // 20px
+        lg: '1.38rem',
+        xl: '1.625rem',
+        '2xl': '2rem', // 32px
+        '5xl': '3rem', 
     }
   },
   plugins: [
