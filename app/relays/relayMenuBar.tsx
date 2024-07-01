69a100a8053ed811eb4262f9d5ea34a20a8c8256
jeremyd <4072+jeremyd@users.noreply.github.com>
2024-07-01 09:53:54 -0700

    explorerv2 experiment menubar etc

---
+++ app/relays/relayMenuBar.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { RelayWithEverything } from "../components/relayWithEverything"
+import { useState } from "react"
+import RelaySmall from "../components/relaySmall"
+
+export default function RelayMenuBar(
+    props: React.PropsWithChildren<{
+        relays: RelayWithEverything[];
+    }>) {
+
+    const [results, setResults] = useState(props.relays)
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        const targetToLower = e.target.value.toLowerCase()
+        const r = props.relays.filter((relay) => {
+            if (relay.name.toLowerCase().includes(targetToLower)) {
+                return true
+            }
+
+            if (relay.details && relay.details.toLowerCase().includes(targetToLower)) {
+                return true
+            }
+            return false
+        })
+        setResults(r)
+    }
+
+
+    //maybe try flex grow
+
+    return (
+        <div className="">
+            <div className="">
+                <div>
+                    <div className="items-center justify-center lg:justify-left lg:items-left">
+                        <input className="input input-bordered" placeholder="Search" onChange={(e) => handleSearch(e)} />
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-8 flex-col">
+                {results.map((relay) => (
+                    <RelaySmall key={"pub" + relay.id} relay={relay} />
+                ))}
+            </div>
+        </div>
+    )
+}
