bc54e3ec37d8951753e4a4b89f4e005eeeec3a60
jeremyd <4072+jeremyd@users.noreply.github.com>
2024-07-22 15:09:43 -0700

    adding invoice notifier for super admin, updating deps

---
+++ app/invoices/adminInvoices.tsx
@@ -0,0 +1,264 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import Bolt11Invoice from "../components/invoice";
+import { use, useState } from "react";
+import { useRouter } from "next/navigation";
+import { nip19 } from "nostr-tools";
+import NDK, { NDKEvent, NDKNip07Signer, NDKPublishError, NDKRelay, NDKRelayAuthPolicies, NDKAuthPolicy, NDKRelaySet, NDKSubscription } from "@nostr-dev-kit/ndk";
+import { getRelayListForUser, getRelayListForUsers } from "@nostr-dev-kit/ndk";
+
+function copyToClipboard(e: any, bolt: string) {
+    e.preventDefault();
+    navigator.clipboard.writeText(bolt).then(() => {
+        console.log("Copied to clipboard!");
+    });
+}
+
+export async function alby(lnurl: string) {
+    // const lnurl = (provided by your application backend)
+    try {
+        await (window as any).webln.enable();
+        const result = await (window as any).webln.sendPayment(lnurl); // promise resolves once the LNURL process is finished
+    } catch (error) {
+        console.log("something went wrong with webln: " + error);
+    }
+}
+
+const ndk = new NDK({
+ //   signer: nip07signer,
+    autoConnectUserRelays: false,
+    enableOutboxModel: false,
+});
+
+const ndkPool = ndk.pool;
+
+export default function AdminInvoices(
+    props: React.PropsWithChildren<{
+        RelayBalances: any;
+        IsAdmin: boolean;
+    }>
+) {
+    const router = useRouter();
+    const [showOrders, setShowOrders] = useState("");
+
+    let useAmount = "";
+
+    async function getTopUpInvoice(b: any) {
+        if(useAmount == "") {
+          useAmount = Math.abs(amountPrecision(b.balance)).toString()
+        }
+        const response = await fetch(
+            `/api/invoices?relayname=${b.relayName}&topup=true&sats=${useAmount}`
+        );
+        const responseJson = await response.json();
+        console.log(responseJson);
+
+        if (response.ok) {
+            router.push(
+                `/invoices?relayname=${b.relayName}&order_id=${responseJson.order_id}&pubkey=unknown&sats=${useAmount}`
+            );
+        }
+    }
+
+    function showOrdersFor(b: any) {
+        if (showOrders === b) {
+            return true;
+        }
+        return false;
+    }
+
+    const handleNotifyUser = async(pubkey: string) => {
+        const nip07signer = new NDKNip07Signer();
+        const activeUser = await nip07signer.blockUntilReady();
+        ndk.signer = nip07signer;
+        ndk.addExplicitRelay("wss://purplepag.es");
+        ndk.addExplicitRelay("wss://nostr21.com");
+        ndk.addExplicitRelay("wss://relay.damus.io");
+        ndk.addExplicitRelay("wss://nos.lol");
+        const recipient = ndk.getUser({pubkey: pubkey});
+        console.log("getting recipient profile")
+        await recipient.fetchProfile();
+        console.log("got recipient profile")
+        console.log("getting relay list for recipient", recipient)
+        let relayList = await getRelayListForUser(recipient.pubkey, ndk);
+        const recipientProfile = recipient.profile;
+        const newEvent = new NDKEvent(ndk);
+        newEvent.kind = 1;
+        let recipientName = "";
+        if (recipientProfile && recipientProfile.name) {
+            recipientName = recipientProfile.name;
+        }
+        newEvent.content = "Hello " + "nostr:" + nip19.npubEncode(pubkey) + " Please visit https://relay.tools/invoices to top up your balance.";
+        newEvent.tag(recipient, "mention");
+
+        ndk.on("event:publish-failed", (event: NDKEvent, error: NDKPublishError, relays: any) => {
+            console.log("event publish failed", event, error);
+            console.log("event publish failed to send to all relays:", relays);
+        });
+
+        const howMany = relayList.relaySet.size;
+        const publishedTo = await newEvent.publish(relayList.relaySet, 5000, howMany);
+        console.log("event was published to: ", publishedTo);
+    }
+
+    //const sortedRelays = props.RelayBalances.sort((a: any, b: any) => a.owner.localeCompare(b.owner));
+    const sortedRelays = props.RelayBalances
+    .filter((relayBalance: any) => relayBalance.relayStatus === 'running')
+    .sort((a: any, b: any) => {
+        const ownerComparison = a.owner.localeCompare(b.owner);
+        return ownerComparison !== 0 ? ownerComparison : a.balance - b.balance;
+    });
+
+    function amountPrecision(amount: number) {
+        let x = Math.round(amount)
+        return x;
+    }
+
+    return (
+        <div>
+            <h1>SUPER ADMIN - Balances</h1>
+            <div className="mt-4">
+                {sortedRelays.map((b: any) => (
+                    <div
+                        key={b.relayId + "rowkey"}
+                        className="flex flex-col border mb-4 bg-gradient-to-r from-accent to-base-100 p-4 "
+                    >
+                        <div className="flex">
+                            <div className="w-1/2 border-b">Relay Name</div>
+                            <div className="w-1/2 border-b text-lg">{b.relayName}</div>
+                        </div>
+                        {props.IsAdmin && 
+                        <div className="flex">
+                            <div className="w-1/2">Relay ID</div>
+                            <div className="w-1/2">{b.relayId}</div>
+                        </div>
+                        }
+                        <div className="flex">
+                            <div className="w-1/2">Relay Status</div>
+                            <div className="w-1/2">{b.relayStatus}</div>
+                        </div>
+                        <div className="flex">
+                            <div className="w-1/2">Paid Relay Bonus</div>
+                            <div className="w-1/2">{b.clientPayments} sats</div>
+                        </div>
+                        <div className="flex">
+                            <div className="w-1/2">Remaining Balance</div>
+                            <div className="w-1/2">
+                                {amountPrecision(b.balance)} sats
+                            </div>
+                        </div>
+                        <div className="flex">
+                            {props.IsAdmin && (
+                                <div className="w-1/2">Owner (pubkey)</div>
+                            )}
+                            {props.IsAdmin && (
+                                <div className="w-1/2">{nip19.npubEncode(b.owner)}</div>
+                            )}
+                        </div>
+                        <div className="flex mt-4">
+                            <button
+                                className="mr-2 btn btn-secondary"
+                                onClick={() => setShowOrders(b.relayId)}
+                            >
+                                show orders
+                            </button>
+                        </div>
+                        <div className="flex mt-4">
+                            <button
+                                className="mr-2 btn btn-secondary"
+                                onClick={(e) => handleNotifyUser(b.owner)}
+                            >
+                            Send Balance Notify
+                            </button>
+                        </div>
+                        
+                        <div className="flex mt-4">
+                            <input
+                                type="text"
+                                name="satsamount"
+                                className="input input-bordered input-primary w-full max-w-xs"
+                                placeholder={Math.abs(amountPrecision(b.balance)).toString()}
+                                onChange={event => {useAmount = event.target.value}}
+                            />
+                            <label className="label">sats</label>
+                            <button
+                                className="btn uppercase btn-secondary"
+                                onClick={() => getTopUpInvoice(b)}
+                            >
+                                top up
+                            </button>
+                        </div>
+
+                        {showOrdersFor(b.relayId) &&
+                            b.unpaidOrders.map((order: any) => (
+                                <div key={order.id + "colkey"} className="flex-col border">
+                                    <div className="flex">
+                                    <div className="w-1/3 mr-2">
+                                        Pending Order
+                                    </div>
+                                    <div className="w-1/3 mr-2">
+                                        {order.amount} sats
+                                    </div>
+                                    <a
+                                        className="btn uppercase btn-secondary"
+                                        href={`/invoices?relayname=${b.relayName}&pubkey=${b.pubkey}&order_id=${order.id}`}
+                                    >
+                                        show
+                                    </a>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2 mr-2 text-lg">
+                                            {b.relayName}
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2">Expires At</div>
+                                        <div className="w-1/2">
+                                            {order.expires_at
+                                                ? new Date(
+                                                      order.expires_at
+                                                  ).toLocaleString()
+                                                : ""}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+
+                        {showOrdersFor(b.relayId) &&
+                            b.orders.map((order: any) => (
+                                <div
+                                    key={order.id + "colkey"}
+                                    className="flex flex-col border"
+                                >
+                                    <div className="flex">
+                                        <div className="w-1/2 mr-2">
+                                            Paid Order
+                                        </div>
+                                        <div className="w-1/2 mr-2">
+                                            {amountPrecision(order.amount)} sats
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2 mr-2 text-lg">
+                                            {b.relayName}
+                                        </div>
+                                    </div>
+                                    <div className="flex">
+                                        <div className="w-1/2">Paid At</div>
+                                        <div className="w-1/2">
+                                            {order.paid_at
+                                                ? new Date(
+                                                      order.paid_at
+                                                  ).toLocaleString()
+                                                : ""}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
