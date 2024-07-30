ad04c9420b1ddcaf6aca9b99b3a4524a2dc5b9a4
jeremyd <jeremyd>
2024-07-30 12:19:09 -0700

    add paused support for relays, added superadmin notify for paused relays

--- pages/api/relay/[id]/settings.ts
+++ pages/api/relay/[id]/settings.ts
@@ -1,5 +1,6 @@
 import prisma from '../../../../lib/prisma'
 import { checkSessionForRelay } from "../../../../lib/checkSessionForRelay"
+import { checkSessionForSuperAdmin } from '../../../../lib/checkSessionForSuperAdmin'
 
 export default async function handle(req: any, res: any) {
     // check owner and relay, to create blank BlockList
@@ -10,12 +11,16 @@ export default async function handle(req: any, res: any) {
     }
 
     if (req.method == "POST") {
-
         const updateFields: { [key: string]: any } = {};
         for (let key in req.body) {
             if (req.body[key] !== undefined) {
                 if(key == "payment_amount") {
                     updateFields[key] = parseInt(req.body[key]);
+                } else if(key == "status") {
+                    // update status only if superadmin
+                    if(await checkSessionForSuperAdmin(req, res)) {
+                        updateFields[key] = req.body[key];
+                    }
                 } else {
                     updateFields[key] = req.body[key];
                 }
