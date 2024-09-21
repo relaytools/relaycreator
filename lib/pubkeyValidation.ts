9461ae37191f1afeff2523228eabe2ec457ef31c
jeremyd <jeremyd>
2024-09-21 12:07:57 -0700

    switching out npub1 regex for bech32 standard in pubkeyValidation

--- lib/pubkeyValidation.ts
+++ lib/pubkeyValidation.ts
@@ -1,12 +1,13 @@
 import { nip19 } from "nostr-tools";
 
 export const convertOrValidatePubkey = (pubkey: string): string | undefined => {
- const lower = pubkey.toLowerCase();
- if (/^[0-9a-f]{64}$/.test(lower)) return pubkey;
- try {
-  const { data } = nip19.decode(lower);
-  return typeof data === 'string' ? data : undefined;
- } catch {
-  return undefined;
- }
-}
+    const lower = pubkey.toLowerCase();
+    if (/npub1[023456789acdefghjklmnpqrstuvwxyz]{6,}/.test(lower))
+        return pubkey;
+    try {
+        const { data } = nip19.decode(lower);
+        return typeof data === "string" ? data : undefined;
+    } catch {
+        return undefined;
+    }
+};
