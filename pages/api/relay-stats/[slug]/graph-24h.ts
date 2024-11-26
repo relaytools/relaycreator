e1bca5275b0cbc96e0cc0aee51db360d99c3706b
jeremyd <4072+jeremyd@users.noreply.github.com>
2024-11-26 12:36:01 -0800

    fix the slug!

--- pages/api/relay-stats/[slug]/graph-24h.ts
+++ pages/api/relay-stats/[slug]/graph-24h.ts
@@ -1,41 +1,36 @@
-import { getInfluxDBClient } from "../../../../lib/influxDBClient";
+import { getInfluxDBClient } from '../../../../lib/influxDBClient'
 
 export default async function handle(req: any, res: any) {
-    const slug = req.query.slug;
+  const slug = req.query.slug;
 
-    if (slug == null) {
-        res.status(404).json({ error: "relay not found" });
-        return;
-    }
+  if(slug == null) {
+    res.status(404).json({"error": "relay not found"})
+    return
+  }
+  
+  if (!process.env.INFLUXDB_URL || !process.env.INFLUXDB_TOKEN || !process.env.INFLUXDB_ORG || !process.env.INFLUXDB_BUCKET) {
+    return Response.json({ stats: null })
+  }
 
-    if (
-        !process.env.INFLUXDB_URL ||
-        !process.env.INFLUXDB_TOKEN ||
-        !process.env.INFLUXDB_ORG ||
-        !process.env.INFLUXDB_BUCKET
-    ) {
-        return Response.json({ stats: null });
-    }
+  try {
+    const influxDB = getInfluxDBClient()
+    const queryApi = influxDB.getQueryApi(process.env.INFLUXDB_ORG)
 
-    try {
-        const influxDB = getInfluxDBClient();
-        const queryApi = influxDB.getQueryApi(process.env.INFLUXDB_ORG);
-
-        const fluxQuery = `
+const fluxQuery = `
       from(bucket: "${process.env.INFLUXDB_BUCKET}")
       |> range(start: -24h)
       |> filter(fn: (r) => r["_measurement"] == "events1")
       |> filter(fn: (r) => r["_field"] == "allowed")
       |> group(columns: ["_measurement", "_field", "relay", "kind"])
-      |> filter(fn: (r) => r["relay"] == ${slug})
+      |> filter(fn: (r) => r["relay"] == "${slug}")
       |> aggregateWindow(every: 1h, fn: count)
       |> filter(fn: (r) => r["_value"] > 0)
       |> yield(name: "count")
       
-    `;
-        const result = await queryApi.collectRows(fluxQuery);
-        return res.status(200).json({ stats: result });
-    } catch (e) {
-        return res.status(200).json({ stats: [] });
-    }
+    `
+    const result = await queryApi.collectRows(fluxQuery)
+    return res.status(200).json({ stats: result })
+  } catch (e) {
+    return res.status(200).json({ stats: [] })
+  }
 }
