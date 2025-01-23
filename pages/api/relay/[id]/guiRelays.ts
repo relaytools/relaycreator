5f32b3de62172c768240a3194f95adc0afa8ebe7
jeremyd <jeremyd>
2025-01-22 17:06:36 -0800

    [trex] landing page change to use api instead of props

---
+++ pages/api/relay/[id]/guiRelays.ts
@@ -0,0 +1,54 @@
+import prisma from "../../../../lib/prisma";
+
+export default async function handle(req: any, res: any) {
+
+  const publicRelays = await prisma.relay.findMany({
+    where: {
+      status: "running",
+      listed_in_directory: true,
+    },
+    select: {
+        name: true,
+        domain: true,
+        is_external: true,
+        details:true,
+        id: true,
+    }
+  })
+
+  const relay = await prisma.relay.findFirst({
+    where: {
+      name: req.query.id,
+      OR: [
+        { status: "running" },
+        { status: "provision" },
+      ],
+    },
+    include: {
+      owner: true,
+      moderators: {
+        include: { user: true },
+      },
+      block_list: {
+        include: {
+          list_keywords: true,
+          list_pubkeys: true,
+          list_kinds: true,
+        },
+      },
+      allow_list: {
+        include: {
+          list_keywords: true,
+          list_pubkeys: true,
+          list_kinds: true,
+        },
+      },
+    }
+  })
+
+  if (!relay) {
+    return res.status(400).json({"error": "relay not found"})
+  }
+
+  return res.status(200).json({ relay, publicRelays })
+}
