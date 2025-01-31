ab6a8b871dc3b885373c06549381c3032d9f6a30
jeremyd <4072+jeremyd@users.noreply.github.com>
2025-01-31 05:05:34 -0800

    [haproxy] client timeout adjust. [lists] list mgmt improvements

--- pages/api/relay/[id]/allowlistpubkeys.ts
+++ pages/api/relay/[id]/allowlistpubkeys.ts
@@ -1,16 +1,15 @@
-import prisma from '../../../../lib/prisma'
-import { checkSessionForRelay } from "../../../../lib/checkSession"
-import { getSession } from 'next-auth/react'
+import prisma from "../../../../lib/prisma";
+import { checkSessionForRelay } from "../../../../lib/checkSession";
 
 // used for bulk addition of pubkeys
 export default async function handle(req: any, res: any) {
     // check owner and relay, to create blank AllowList
-    const isMyRelay = await checkSessionForRelay(req, res, true)
+    const isMyRelay = await checkSessionForRelay(req, res, true);
     if (isMyRelay == null) {
-        return
+        return;
     }
 
-    let allow_list = isMyRelay.allow_list
+    let allow_list = isMyRelay.allow_list;
 
     if (req.method == "POST") {
         const { pubkeys, reason } = req.body;
@@ -18,65 +17,74 @@ export default async function handle(req: any, res: any) {
             const newa = await prisma.allowList.create({
                 data: {
                     relayId: isMyRelay.id,
-                }
-            })
-            allow_list = newa
+                },
+            });
+            allow_list = newa;
         }
 
         if (allow_list == null) {
-            res.status(500).json({ "error": "allow_list is null" })
-            return
+            res.status(500).json({ error: "allow_list is null" });
+            return;
         }
 
         if (reason == null) {
-            res.status(500).json({ "error": "reason is null" })
-            return
+            res.status(500).json({ error: "reason is null" });
+            return;
         }
 
         // if list of pubkeys has reason = "list:<something>" then sync the list.
         // by deleting the pubkeys that are not in the list anymore
-        if(reason.startsWith("list:")) {
+        if (reason.startsWith("list:")) {
             const curPubkeys = await prisma.listEntryPubkey.deleteMany({
                 where: {
                     reason: reason,
-                    AllowListId: allow_list.id
-                }
-            })
+                    AllowListId: allow_list.id,
+                },
+            });
         }
 
-        let newPubkeys = []
+        let newPubkeys = [];
         for (const pk of pubkeys) {
             const newp = await prisma.listEntryPubkey.create({
                 data: {
                     AllowListId: allow_list.id,
                     pubkey: pk,
                     reason: reason,
-                }
-            })
-            newPubkeys.push(newp)
+                },
+            });
+            newPubkeys.push(newp);
         }
-        res.status(200).json({ "pubkeys": newPubkeys });
-    // DELETE ALL
+        res.status(200).json({ pubkeys: newPubkeys });
+        // DELETE ALL or LIST
     } else if (req.method == "DELETE") {
         if (allow_list == null || allow_list.id == null) {
-            res.status(500).json({ "error": "allow_list is null" })
-            return
+            res.status(500).json({ error: "allow_list is null" });
+            return;
         }
 
         const listId = req.query.list_id;
-        if (listId == null || listId != "all") {
-            res.status(500).json({ "error": "no list_id == all" })
-            return
+        if (listId == null) {
+            res.status(500).json({ error: "no list_id" });
+            return;
         }
-
-        await prisma.listEntryPubkey.deleteMany({
-            where: {
-                AllowListId: allow_list.id
-            }
-        })
+        // delete all lists with this reason..
+        if (listId != "") {
+            await prisma.listEntryPubkey.deleteMany({
+                where: {
+                    AND: [{ AllowListId: allow_list.id }, { reason: { contains: listId }}],
+                },
+            });
+            // delete all
+        } else if (listId == "all") {
+            await prisma.listEntryPubkey.deleteMany({
+                where: {
+                    AllowListId: allow_list.id,
+                },
+            });
+        }
 
         res.status(200).json({});
     } else {
-        res.status(500).json({ "error": "method not allowed" })
+        res.status(500).json({ error: "method not allowed" });
     }
 }
