34be3955055b14e2c0b8129abebee5b598a53465
jeremyd <jeremyd>
2025-06-10 11:17:25 -0700

    tryin out clientinvoices UI progress1

---
+++ app/clientinvoices/paymentSuccess.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { useEffect, useState } from 'react'
+import ZapAnimation from '../lightningsuccess/lightning'
+import TextStringWaitingForPayment from '../components/textStringWaitingForPayment';
+
+export default function PaymentSuccess(props: React.PropsWithChildren<{
+    signed_in: boolean;
+    relay_id: string;
+    payment_hash: string;
+    payment_request: string;
+    order_id: string;
+}>) {
+
+    const [status, setStatus] = useState(false)
+
+    const getInvoiceStatus = async () => {
+        const result = await fetch(`/api/clientorders/${props.order_id}`)
+        const j = await result.json()
+        setStatus(j.clientOrder.paid)
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getInvoiceStatus();
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <>
+            {status && props.signed_in && <div>success<ZapAnimation redirect_to={`/clientinvoices`}></ZapAnimation></div>}
+            {status && !props.signed_in && <div>success<ZapAnimation redirect_to={`/clientinvoices}`}></ZapAnimation></div>}
+
+            {!status && <div> 
+                <TextStringWaitingForPayment />
+            </div>}
+        </>
+    )
+}
