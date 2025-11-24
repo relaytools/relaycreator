504d3a83a7b04500288607891285014c7274399d
jeremyd <jeremyd>
2025-11-24 12:08:21 -0800

    attempting to fix influxdb connection stats timeouts

--- lib/influxDBClient.ts
+++ lib/influxDBClient.ts
@@ -7,7 +7,8 @@ export function getInfluxDBClient() {
         if (!process.env.INFLUXDB_URL || !process.env.INFLUXDB_TOKEN) {
             throw new Error('InfluxDB environment variables are not set');
         }
-        influxDB = new InfluxDB({ url: process.env.INFLUXDB_URL, token: process.env.INFLUXDB_TOKEN, timeout: 20000 });
+        // Reduced timeout to 8s
+        influxDB = new InfluxDB({ url: process.env.INFLUXDB_URL, token: process.env.INFLUXDB_TOKEN, timeout: 8000 });
     }
     return influxDB;
 }
